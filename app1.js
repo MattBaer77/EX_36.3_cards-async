8772354d95d2f4a156c2159f03098321fa2159b8
Matthew Baer <matt.baer77@gmail.com>
2023-05-26 00:17:25 -0400

    finished part 1

--- app1.js
+++ app1.js
@@ -2,34 +2,62 @@ const numberFactList = document.querySelector('.facts-list')
 const numbersBaseURL = 'http://numbersapi.com'
 const favNumber = 55;
 
+const favNumbers = [66, 666, 6666]
+
 // 1
-axios.get(`${numbersBaseURL}/${favNumber}?json`)
-.then(data => {
-    console.log(data);
-})
+async function getFact(favoriteNumber) {
+
+    let fact = await axios.get(`${numbersBaseURL}/${favNumber}?json`);
+    return fact.data.text
+
+}
 
 // 2
+async function getAndPrintMany(favoriteNumbers) {
 
-const favNumbers = [66, 666, 6666]
-axios.get(`${numbersBaseURL}/${favNumbers}?json`)
-.then(data => {
-    console.log(data);
-})
+    let facts = [];
+
+    for (let number of favoriteNumbers) {
+        let fact = await axios.get(`${numbersBaseURL}/${favNumber}?json`);
+        facts.push(fact.data.text)
+    }
+
+    for (let fact of facts) {
+        console.log(fact)
+    }
+}
 
 // 3
-Promise.all(
-    Array.from ({ length: 4 }, () => {
-    return axios.get(`${numbersBaseURL}/${favNumber}?json`);
-})
-).then(facts => {
-    facts.forEach(data => {
+async function addFactsToPage(favoriteNumber, numberOfFacts) {
 
-        console.log(data)
+    let facts = [];
 
-        newListItem = document.createElement('li')
-        newListItem.innerText = data.data.text
+    for (
+        let i = 0;
+        i < numberOfFacts;
+        i++
+    ) {
+        fetchedFact = await getFact(favoriteNumber);
+        facts.push(fetchedFact)
+    }
+
+    facts.forEach(fact => {
+
+        newListItem = document.createElement('li');
+        newListItem.innerText = fact;
         numberFactList.appendChild(newListItem)
 
     })
 
-});
+}
+
+async function start() {
+
+    let singleToPrint = await getFact(favNumber)
+    console.log(singleToPrint)
+    getAndPrintMany(favNumbers)
+    addFactsToPage(favNumber, 4)
+
+}
+
+start()
