f5cfe25f1c936e90325b73102da57f02208be394
Matthew Baer <matt.baer77@gmail.com>
2023-05-26 01:18:26 -0400

    finished part 2

--- app2.js
+++ app2.js
@@ -1,82 +1,92 @@
-// // Global Variables
-// const draw_button = document.querySelector('.draw-button');
-// const cardArea = document.querySelector('.card-area');
-// const cardsBaseURL = 'https://deckofcardsapi.com/api/deck';
+// Global Variables
+const drawButton = document.querySelector('.draw-button');
+const cardArea = document.querySelector('.card-area');
+const cardsBaseURL = 'https://deckofcardsapi.com/api/deck';
+let pageDeckId;
+let previousOffset=0;
 
-// // Create Card HTML
-// function createCardHTML(imgSrc, offset) {
-//     // const HTML = `<img class="card" src="${imgSrc}" alt="" style="translate: 0px ${offset}px;">`
-//     // return HTML
+// Create Card HTML
+function createCardHTML(imgSrc, offset) {
 
-//     const newCard = document.createElement('img')
-//     newCard.classList.add('card')
-//     newCard.src = imgSrc
-//     newCard.style.translate = `0px ${offset}px`
+    const newCard = document.createElement('img')
+    newCard.classList.add('card')
+    newCard.src = imgSrc
+    newCard.style.translate = `0px ${offset}px`
 
-//     return newCard
+    return newCard
 
-// }
+}
 
-// // On Load
-// window.addEventListener("load", (e) => {
+async function shuffleReturnDeckID() {
+    const deck = await axios.get(`${cardsBaseURL}/new/shuffle`);
+    return deck.data.deck_id
+}
 
-//     // 1
-//     axios.get(`${cardsBaseURL}/new/shuffle`)
-//     .then(res => {
-//         return axios.get(`${cardsBaseURL}/${res.data.deck_id}/draw`)
-//     })
-//     .then(res => {
-//         console.log(`${res.data.cards[0].value} of ${res.data.cards[0].suit}`)
-//     })
+function stringFromCard(res) {
 
-//     // 2
-//     // Variables just for 2
-//     let deckId2;
-//     let cardOneString;
-//     let cardTwoString;
+    return `${res.data.cards[0].value} of ${res.data.cards[0].suit}`
 
-//     // 2 Call
-//     axios.get(`${cardsBaseURL}/new/shuffle`)
-//     .then(res => {
-//         deckId2 = res.data.deck_id
-//         return axios.get(`${cardsBaseURL}/${deckId2}/draw`)
-//     })
-//     .then(res => {
-//         cardOneString = `${res.data.cards[0].value} of ${res.data.cards[0].suit}`
-//         return axios.get(`${cardsBaseURL}/${deckId2}/draw`)
-//     })
-//     .then(res => {
-//         cardTwoString = `${res.data.cards[0].value} of ${res.data.cards[0].suit}`
-//         console.log(cardOneString)
-//         console.log(cardTwoString)
-//     })
+}
 
-//     // 3
-//     let deck_id = null;
-//     let previousOffset = 0;
+// 1
+async function getSingle(deckId) {
 
-//     // Get Deck ID - Show draw button.
-//     axios.get(`${cardsBaseURL}/new/shuffle`)
-//     .then(res => {
-//         deck_id = res.data.deck_id;
-//         draw_button.style.display = "block"
-//     })
+    const res = await axios.get(`${cardsBaseURL}/${deckId}/draw`);
 
-//     // Event Listener for button click.
-//     draw_button.addEventListener('click', (e) => {
-//         axios.get(`${cardsBaseURL}/${deck_id}/draw/`).then(({data}) => {
+    return res
 
-//             let imgSrc = data.cards[0].image;
-//             let offset = previousOffset + 60;
+}
 
-//             let newCard = createCardHTML(imgSrc,offset);
-//             cardArea.appendChild(newCard);
-//             previousOffset = offset;
+// 2
+async function getMultiple(deckId, numberToGet) {
+    
+    const cards = [];
 
-//             if (data.remaining === 0) {
-//                 draw_button.style.display = "none"
-//             }
-//         })
-//     })
+    for (
+        let i = 0;
+        i < numberToGet;
+        i++
+    ) {
+        const fetchedCard = await getSingle(deckId)
+        cards.push(fetchedCard)
+    }
 
-// })
+    return cards
+}
+
+async function start() {
+
+    const deckId = await shuffleReturnDeckID()
+    const singleCard = await getSingle(deckId)
+    const singleCardString = stringFromCard(singleCard)
+    console.log(singleCardString)
+
+    const deck2Id = await shuffleReturnDeckID()
+    const multipleCards = await getMultiple(deck2Id, 2)
+    for (let card of multipleCards) {console.log(stringFromCard(card))}
+
+    pageDeckId = await shuffleReturnDeckID()
+
+    // Event Listener for button click.
+    drawButton.addEventListener('click', async (e) => {
+
+        let fetchedCard = await getSingle(pageDeckId)
+
+        let imgSrc = fetchedCard.data.cards[0].image;
+        let offset = previousOffset + 60;
+
+        let newCard = createCardHTML(imgSrc,offset);
+        cardArea.appendChild(newCard);
+        previousOffset = offset;
+
+        if (fetchedCard.data.remaining === 0) {
+            drawButton.style.display = "none"
+        }
+
+    })
+
+    drawButton.style.display="block"
+
+}
+
+start()
